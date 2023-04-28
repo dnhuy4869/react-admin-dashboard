9ee7bdb78ce27b66de246acfbcbcef323a5fb200
Huy <73681373+dnhuy4869@users.noreply.github.com>
2023-04-28 19:01:42 +0700

    folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <>
+            <div className="app">
+                App
+            </div>
+        </>
+    )
+}
+
+export default App;
