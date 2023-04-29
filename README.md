cab83722ea638425bc965436681ff69b36630e86
Huy <73681373+dnhuy4869@users.noreply.github.com>
2023-04-29 20:31:03 +0700

    light and dark mode

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Install dependencies:
 
-### `npm i @mui/material @emotion/react @emotion/styled @mui/x-data-grid @mui/icons-material react-router-dom@6 react-pro-sidebar formik yup @fullcalendar/core @fullcalendar/daygrid @fullcalendar/timegrid @fullcalendar/list @nivo/core @nivo/pie @nivo/line @nivo/bar @nivo/geo`
+### `npm i @mui/material @emotion/react @emotion/styled @mui/x-data-grid @mui/icons-material react-router-dom@6 react-pro-sidebar@0.7.1 formik yup @fullcalendar/core @fullcalendar/daygrid @fullcalendar/timegrid @fullcalendar/list @nivo/core @nivo/pie @nivo/line @nivo/bar @nivo/geo`
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
 const App = () => {
+    const [theme, colorMode] = useMode();
+
     return (
         <>
-            <div className="app">
-                App
-            </div>
+            <ColorModeContext.Provider value={colorMode}>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <div className="app">
+                        <main className="content">
+                            <Topbar />
+                        </main>
+                        
+                    </div>
+                </ThemeProvider>
+            </ColorModeContext.Provider>
         </>
     )
 }
